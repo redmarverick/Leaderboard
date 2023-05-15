b5f6b0711bbad9a26e27c827e0e50eca8a9399a5
Nylbert Victor <nyldrift@hotmail.com>
2023-05-15 09:50:26 -0300

    Corrections
    - webpack correction to add style and html source
    - Linters reinstalled
    - added default font
    - added button type

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
-const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),  
-    filename: 'output.bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'output.bundle.js',
+  },
+  module: {
+    rules: [
+      // CSS and style loader configuration
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      // Other rules...
+    ],
   },
   plugins: [
-      new HtmlWebPackPlugin()
-   ]
+    new HtmlWebPackPlugin({
+      title: 'Output Management',
+      template: './src/index.html',
+    }),
+  ],
 };
